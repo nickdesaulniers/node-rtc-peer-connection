2a9c9b6f7f97390f3fd52a61f1887a79519c3e7e
Nick Desaulniers <ndesaulniers@mozilla.com>
2015-12-03 15:27:32 -0800

    implement constructor

--- RTCPeerConnection.js
+++ RTCPeerConnection.js
@@ -2,7 +2,22 @@ var SDP = require('./sdp');
 var promisify = require('promisify-node');
 var ipInfo = promisify(require('./ip_info'));
 
-function RTCPeerConnection () {};
+// https://w3c.github.io/webrtc-pc/#idl-def-RTCPeerConnection
+function RTCPeerConnection (configuration) {
+  // for debugging, will contain internal/external ip and ports after
+  // createOffer
+  this._info = null;
+  // https://w3c.github.io/webrtc-pc/#dom-peerconnection
+  this.setConfiguration(configuration);
+  this.signalingState = 'stable';
+  this.iceConnectionState = 'new';
+  this.iceGatheringState = 'new';
+  this.pendingLocalDescription = null;
+  this.currentLocalDescription = null;
+  this.pendingRemoteDescription = null;
+  this.currentRemoteDescription = null;
+  this._operations = [];
+};
 
 function constructSDPFromInfo (info) {
   this._info = info;
@@ -20,6 +35,11 @@ function constructSDPFromInfo (info) {
 
 RTCPeerConnection.prototype.createOffer = function () {
   return ipInfo().then(constructSDPFromInfo.bind(this));
+};
+
+RTCPeerConnection.prototype.getConfiguration = function () {};
+RTCPeerConnection.prototype.setConfiguration = function (configuration) {
+  this.configuration = configuration;
 };
 
 module.exports = RTCPeerConnection;
