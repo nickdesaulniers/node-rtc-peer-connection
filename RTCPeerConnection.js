3aa322b9dbc1b174cf8f865a8f38244ff5c2ad64
Nick Desaulniers <ndesaulniers@mozilla.com>
2015-12-03 15:13:34 -0800

    implement createOffer

--- RTCPeerConnection.js
+++ RTCPeerConnection.js
@@ -1,4 +1,26 @@
+var SDP = require('./sdp');
+var promisify = require('promisify-node');
+var ipInfo = promisify(require('./ip_info'));
+
 function RTCPeerConnection () {};
 
-RTCPeerConnection.prototype.createOffer = function () {};
+function constructSDPFromInfo (info) {
+  this._info = info;
+
+  var sdp = new SDP;
+  sdp.setExternalAddr(info.external.addr);
+  sdp.setExternalPort(info.external.port);
+  // TODO: set internal ports?
+
+  return {
+    sdp: sdp.toString(),
+    type: 'offer',
+  };
+};
+
+RTCPeerConnection.prototype.createOffer = function () {
+  return ipInfo().then(constructSDPFromInfo.bind(this));
+};
+
+module.exports = RTCPeerConnection;
 
