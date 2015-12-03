a927b5c34e08b69084b72c012b8089bbed916526
Nick Desaulniers <ndesaulniers@mozilla.com>
2015-12-03 14:06:33 -0800

    get rid of my.js

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+var iceServers = {
+  iceServers: [
+      "stun.l.google.com:19302",
+      //"stun1.l.google.com:19302",
+      //"stun2.l.google.com:19302",
+      //"stun3.l.google.com:19302",
+      //"stun4.l.google.com:19302",
+      //"stun.ekiga.net",
+      //"stun.ideasip.com",
+      //"stun.schlund.de",
+      //"stun.stunprotocol.org:3478",
+      //"stun.voiparound.com",
+      //"stun.voipbuster.com",
+      //"stun.voipstunt.com",
+      //"stun.voxgratia.org",
+      //"stun.services.mozilla.com"
+  ].map(function (url) {
+    return {
+      urls: 'stun:' + url,
+    };
+  }),
+};
+
 function uuid () {
   // http://stackoverflow.com/a/2117523/1027966
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
