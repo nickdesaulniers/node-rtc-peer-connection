4c996bed8fb8fff1d562215fb34aa198e4254751
Nick Desaulniers <ndesaulniers@mozilla.com>
2015-12-09 13:15:23 -0500

    code I forgot to commit

---
+++ ice-ufrag-pwd.js
@@ -0,0 +1,18 @@
+// http://tools.ietf.org/html/rfc5245#section-15.1
+var iceChar = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/';
+
+function getRandomStr (len) {
+  var str = '';
+  for (var i = 0; i < len; ++i) {
+    str += iceChar[Math.random() * iceChar.length | 0];
+  }
+  return str;
+};
+
+// http://tools.ietf.org/html/rfc5245#section-15.4
+
+module.exports = {
+  ufrag: getRandomStr.bind(null, 4),
+  password: getRandomStr.bind(null, 22),
+};
+
